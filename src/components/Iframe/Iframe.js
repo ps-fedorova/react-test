1a4a09d32837442259a191bb4f0e75441bc25966
Polina Fedorova <ps.fedorova@yandex.ru>
2021-02-19 15:14:41 +0500

    fix: fix state for Iframe

--- src/components/Iframe/Iframe.js
+++ src/components/Iframe/Iframe.js
@@ -2,26 +2,27 @@ import React from 'react';
 import s from './Iframe.module.css'
 
 export const IframeInside = ({ message }) => {
-  console.log(`IframeInside: ${message}`)
   return (
     <div className={s.iframeInside}>
-      <h2 id="received-message" style={{ color: "white" }}>
-        {message}
-      </h2>
+      <h2 id="received-message" style={{ color: "white" }}>{message}</h2>
     </div>)
 }
 
 export const Iframe = ({ message, onload }) => {
   const iframeRef = React.useRef();
 
-  const handleSubmit = () => iframeRef.current.contentWindow.postMessage(message, "*");
+  const handleSubmit = () => {
+    iframeRef.current.contentWindow.postMessage(message, "*")
+  };
 
   const onChange = (evt) => {
     onload(evt.target.value);
-    handleSubmit();
   };
 
-  console.log(`Iframe: ${message}`)
+  React.useEffect(() => {
+    handleSubmit();
+  }, [onChange])
+
   return (
     <div className={s.iframe}>
       <iframe ref={iframeRef} title="iframe" src="/#/iframeInside" frameBorder="0" scrolling="no"/>
